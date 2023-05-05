77bb0010a3669c6cee8bf1a29660b2a5acfc06be
Karel Šťastný <karell29@icloud.com>
2023-05-05 14:31:54 +0200

    V JS dodělány všechny addEventylistener + zkontrolovány výpisi zpět do stránky

--- zjisti-heslo.js
+++ zjisti-heslo.js
@@ -1,9 +1,48 @@
-document.querySelector("a").addEventListener("copy")
+//  document.querySelector("a").addEventListener("copy")
 
 
 document.querySelector(".ask-1").addEventListener("click", function(event){
 
-    let answer1 = document.querySelector(".answer-1")
-    answer1.textContent = "1"
+    let answer = document.querySelector(".answer-1")
+    answer.textContent = "1"
 })
 
+document.querySelector(".ask-2").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-2")
+    answer.textContent = "2"
+})
+
+document.querySelector(".ask-3").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-3")
+    answer.textContent = "3"
+})
+
+document.querySelector(".ask-4").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-4")
+    answer.textContent = "4"
+})
+
+document.querySelector(".ask-5").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-5")
+    answer.textContent = "5"
+})
+
+document.querySelector(".ask-6").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-6")
+        answer.textContent = "6"
+})
+
+document.querySelector(".ask-7").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-7")
+        answer.textContent = "7"
+})
+
+document.querySelector(".ask-8").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-8")
+        answer.textContent = "8"
+})
+
+document.querySelector(".ask-9").addEventListener("click", function(event){
+    let answer = document.querySelector(".answer-9")
+        answer.textContent = "9"
+})

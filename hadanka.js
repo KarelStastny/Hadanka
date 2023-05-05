8fc4b1474df5bfd14775aabc2f7eb5e473a53153
Karel Šťastný <karell29@icloud.com>
2023-05-05 13:05:00 +0200

    Upraven SCSS

--- hadanka.js
+++ hadanka.js
@@ -1,3 +1,6 @@
+document.querySelector("a").addEventListener("copy")
+
+
 document.querySelector(".ask-1").addEventListener("click", function(event){
 
     let answer1 = document.querySelector(".answer-1")

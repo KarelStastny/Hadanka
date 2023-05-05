b286b0b59ef7a5d8f97f4fb2ba26a18d801e27bf
Karel Šťastný <karell29@icloud.com>
2023-05-05 15:42:14 +0200

    Dodělán JS na odpovědi a otázky, již funkční na všech 9

--- zjisti-heslo.js
+++ zjisti-heslo.js
@@ -1,20 +1,129 @@
-const mainAnswer = ["Karel", "Petr"]
-const mainAnswerBad = ["Martin", "Tom"]
+const mainAnswer = ["Ron", "Modrá", "Pes", "20", "1 12", "růžová", "značkovací", "32", "40 075"]
+const mainAnswerBad = ["Hermiona", "Zelená", "Kočka", "18", "1 30", "žlutá", "skriptovací", "28", "40 000"]
 
-
+// ====== Ask 1
 document.querySelector(".ask-1").addEventListener("input", function(event){
 
     let ask = event.target.value
     let answer = document.querySelector(".answer-1")
 
-    if(ask.toLowerCase() === mainAnswer[0].toLocaleLowerCase()){
+    if(ask.toLowerCase().trim() === mainAnswer[0].toLocaleLowerCase().trim()){
+        answer.textContent = "7"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[0].toLocaleLowerCase().trim()){
         answer.textContent = "1"
-    }else if (ask.toLowerCase() === mainAnswerBad[0].toLocaleLowerCase()){
-        answer.textContent = "6"
     }else{
-        answer.textContent = "."
+        answer.textContent = "?"
     }
-        
-  
 })
+// ====== Ask 2
+document.querySelector(".ask-2").addEventListener("input", function(event){
 
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-2")
+
+    if(ask.toLowerCase().trim() === mainAnswer[1].toLocaleLowerCase().trim()){
+        answer.textContent = "3"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[1].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 3
+document.querySelector(".ask-3").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-3")
+
+    if(ask.toLowerCase().trim() === mainAnswer[2].toLocaleLowerCase().trim()){
+        answer.textContent = "8"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[2].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 4
+document.querySelector(".ask-4").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-4")
+
+    if(ask.toLowerCase().trim() === mainAnswer[3].toLocaleLowerCase().trim()){
+        answer.textContent = "3"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[3].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 5
+document.querySelector(".ask-5").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-5")
+
+    if(ask.toLowerCase().trim() === mainAnswer[4].toLocaleLowerCase().trim()){
+        answer.textContent = "2"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[4].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 6
+document.querySelector(".ask-6").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-6")
+
+    if(ask.toLowerCase().trim() === mainAnswer[5].toLocaleLowerCase().trim()){
+        answer.textContent = "5"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[5].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 7
+document.querySelector(".ask-7").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-7")
+
+    if(ask.toLowerCase().trim() === mainAnswer[6].toLocaleLowerCase().trim()){
+        answer.textContent = "3"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[6].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 8
+document.querySelector(".ask-8").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-8")
+
+    if(ask.toLowerCase().trim() === mainAnswer[7].toLocaleLowerCase().trim()){
+        answer.textContent = "6"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[7].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
+// ====== Ask 9
+document.querySelector(".ask-9").addEventListener("input", function(event){
+
+    let ask = event.target.value
+    let answer = document.querySelector(".answer-9")
+
+    if(ask.toLowerCase().trim() === mainAnswer[8].toLocaleLowerCase().trim()){
+        answer.textContent = "8"
+    }else if (ask.toLowerCase().trim() === mainAnswerBad[8].toLocaleLowerCase().trim()){
+        answer.textContent = "1"
+    }else{
+        answer.textContent = "?"
+    }
+})
